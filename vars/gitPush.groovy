9c15ba7943fc17cf2c587bc5e0bf839d37eeef93
Rishikvashu <95581558+Rishikvashu@users.noreply.github.com>
2026-05-17 19:30:53 +0530

    Update gitPush.groovy

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,9 +1,18 @@
-def call() {
+def call(branchName) {
 
-    sh '''
-    git branch
-    git add .
-    git commit -m "Updated from Jenkins build" || true
-    git push origin HEAD:master
-    '''
+    withCredentials([usernamePassword(
+        credentialsId: 'github-creds',
+        usernameVariable: 'GIT_USERNAME',
+        passwordVariable: 'GIT_PASSWORD'
+    )]) {
+
+        sh """
+        git config user.name "Rishikvashu"
+        git config user.email "vashishtha123456789@gmail.com"
+
+        git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Rishikvashu/shared-library-1.git
+
+        git push origin ${branchName}
+        """
+    }
 }
